a1548de89e2bf4e3adf54cc76e6cbb79e98f012e
Jai Thawal <thawaljai08@gmail.com>
2026-06-04 00:57:53 +0530

    Add matrixReshape function to reshape a matrix

---
+++ Week_1/Day_4/ReshapeTheMatrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size();
+        int n = mat[0].size();
+        if (m * n != r * c) return mat;
+        vector<vector<int>>ans(r,vector<int>(c));
+        int rind=0;
+        int cind=0;
+        for(int i=0;i<m;i++){
+            for(int j =0;j<n;j++){
+                ans[rind][cind]=mat[i][j];
+                cind++;
+                if(cind==c){
+                    cind=0;
+                    rind++;
+            }
+        }
+        }
+        return ans ;
+    }
+};
