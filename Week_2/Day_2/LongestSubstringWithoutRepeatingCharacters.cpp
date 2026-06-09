12e8878739769568760a419dbf99ed817fb4975b
Jai Thawal <thawaljai08@gmail.com>
2026-06-09 10:38:55 +0530

    Implement lengthOfLongestSubstring function

---
+++ Week_2/Day_2/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> hash(256, -1);
+        int maxi =0;
+        int n = s.size();
+        int l =0;
+        int r = 0;
+        while(r<n){
+            if(hash[s[r]]!=-1){
+                if(hash[s[r]]>=l) l=hash[s[r]]+1;
+            }
+            maxi = max(maxi,r-l+1);
+            hash[s[r]]=r;
+            r=r+1;
+        }
+        return maxi;
+    }
+};
