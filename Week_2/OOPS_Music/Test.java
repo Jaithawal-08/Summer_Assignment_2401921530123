6be26554adfac69054bbaa03140b5464ec89a23a
Jai Thawal <thawaljai08@gmail.com>
2026-06-14 00:01:23 +0530

    Add Saxophone class and demonstrate polymorphism

--- Week_2/OOPS_Music/Test.java
+++ Week_2/OOPS_Music/Test.java
@@ -6,10 +6,13 @@ public class Test {
     public static void main(String[] args) {
         Veena v = new Veena();
         v.play();
+        
         Saxophone s = new Saxophone();
         s.play();
+        
         Playable p1 = v;
         Playable p2 = s;
+        
         p1.play();
         p2.play();
     }
